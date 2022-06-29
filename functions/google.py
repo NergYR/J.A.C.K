092f541d2483b41139d50e993625f6ebf4109317
energetiq <energetiq@outlook.com>
2022-06-29 20:16:17 +0200

    Add : - google Calendar addEvent() - google Calendar getEvents()

--- functions/google.py
+++ functions/google.py
@@ -103,4 +103,19 @@ class Calendar:
 
         event = service.events().insert(calendarId='xzeystape@gmail.com', body=event).execute()
         print('Event created: %s' % (event.get('htmlLink')))
+    def getEvents():
+        service = Calendar.get_calendar_service()
+        now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        print('Getting List o 10 events')
+        events_result = service.events().list(
+            calendarId='xzeystape@gmail.com', timeMin=now,
+            maxResults=10, singleEvents=True,
+            orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            print(start, event['summary'])
 
